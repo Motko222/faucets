dd089d5a828eb80cd147a10893e204b8bf4d6037
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-27 23:34:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd ~/faucets
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+
+cd ~/scripts/$folder
 apt install python3.10-venv
 python3 -m venv venv
 source venv/bin/activate
